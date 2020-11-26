8599227af6b9cb8a1131db6f1740b9a2a6cb88b1
slonofanya <slonofanya@gmail.com>
2020-11-26 10:29:51 +0200

    fixed

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 console.log('-=Start sl ext=-');
 
+const isEnabled = el => (el.getAttribute('class') || '').indexOf('disabled') < 0
+
 setInterval(() => {
   const twichPointsButton = document.querySelector('.tw-button');
 
@@ -8,22 +10,29 @@ setInterval(() => {
     twichPointsButton.click();
   }
 
-  const wasdButtons = document.querySelectorAll('.gifts-info__buttons');
-  console.log({ wasdButtons });
+  const wasdButtons = document.querySelectorAll('.gifts-info__buttons button');
 
   if (wasdButtons) {
     const dron = wasdButtons[0];
 
-    if (dron.nextElementSibling.innerText.indexOf('10 COINS') && dron.getAttribute('class').indexOf('disabled') > -1) {
-      console.log({ dron });
-      dron.click();
+    if (dron) {
+	    const has10 = dron.nextElementSibling.innerText.indexOf('10 COINS');
+	    const isDronEnabled = isEnabled(dron)
+	    console.log({ has10, isDronEnabled});
+
+	    if (has10 && isDronEnabled) {
+	      dron.click();
+	    }
     }
 
     const banka = wasdButtons[2];
 
-    if (banka.getAttribute('class').indexOf('disabled') > -1) {
-      console.log({ banka });
-      banka.click();
+    if (banka) {
+	    const isBankaEnabled = isEnabled(banka)
+
+	    if (isBankaEnabled) {
+	      banka.click();
+	    }
     }
   }
 }, 5000);
