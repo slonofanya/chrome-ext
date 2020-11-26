0484fd5ac36b8e517d0e09ee53eab1718e5d185b
vimelnyk <vimelnyk@deloitte.ua>
2020-11-25 23:47:36 -0800

    added wasd

--- content.js
+++ content.js
@@ -7,4 +7,22 @@ setInterval(() => {
     console.log({ twichPointsButton });
     twichPointsButton.click();
   }
+
+  const wasdButtons = document.querySelector('.gifts-info__buttons');
+
+  if (wasdButtons) {
+    const dron = wasdButtons[0];
+
+    if (dron.nextElementSibling.innerText.indexOf('10 COINS') && dron.getAttribute('class').indexOf('disabled') > -1) {
+      console.log({ dron });
+      dron.click();
+    }
+
+    const banka = wasdButtons[2];
+
+    if (banka.getAttribute('class').indexOf('disabled') > -1) {
+      console.log({ banka });
+      banka.click();
+    }
+  }
 }, 5000);
