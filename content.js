0bd5c6100d9f8e98af763859d1ea948b6995c512
slonofanya <slonofanya@gmail.com>
2021-05-21 19:50:22 +0300

    fix tw

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ console.log('-=Start sl ext=-');
 const isEnabled = el => (el.getAttribute('class') || '').indexOf('disabled') < 0
 
 setInterval(() => {
-  const twichPointsButton = document.querySelector('.tw-button');
+  const twichPointsButton = document.querySelector('[aria-label="Claim Bonus"]');
 
   if (twichPointsButton) {
     console.log({ twichPointsButton });
