c01cbf3ac5364fb0a1a9c4d04da8a9e958d75370
Slava <slonofanya@users.noreply.github.com>
2024-04-24 19:27:11 +0300

    v3

--- content.js
+++ content.js
@@ -1,37 +1,8 @@
-console.log('-=Start sl ext=-');
-
-const isEnabled = el => (el.getAttribute('class') || '').indexOf('disabled') < 0
-
 setInterval(() => {
   const twichPointsButton = document.querySelector('[aria-label="Claim Bonus"]');
 
   if (twichPointsButton) {
     console.log({ twichPointsButton });
     twichPointsButton.click();
-  }
-
-  const wasdButtons = document.querySelectorAll('.gifts-info__buttons button');
-
-  if (wasdButtons) {
-    const dron = wasdButtons[0];
-
-    if (dron) {
-	    const has10 = dron.nextElementSibling.innerText.indexOf('10 COINS') > -1;
-	    const isDronEnabled = isEnabled(dron)
-
-	    if (has10 && isDronEnabled) {
-	      dron.click();
-	    }
-    }
-
-    const banka = wasdButtons[2];
-
-    if (banka) {
-	    const isBankaEnabled = isEnabled(banka)
-
-	    if (isBankaEnabled) {
-	      banka.click();
-	    }
-    }
   }
 }, 5000);
